aa260d126e69405f5a90e6103f7d175ef65c8a07
Andrea Llerena <20201173@aloe.ulima.edu.pe>
2023-03-21 10:30:00 -0500

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Andrea Llerena on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
